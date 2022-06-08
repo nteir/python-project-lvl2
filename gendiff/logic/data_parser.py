51aad597f6c9451d19a49a2bb3a8749105ead7dd
Natalia Teplynina <nt@mnt9.com>
2022-06-08 11:38:32 +0300

    fix lint complaints

--- gendiff/logic/data_parser.py
+++ gendiff/logic/data_parser.py
@@ -5,7 +5,7 @@ import yaml
 def parse_to_dict(data, data_type):
     PARSING_FUNC = {
         'json': json.load,
-        'yaml': lambda data: yaml.load(data, Loader = yaml.Loader),
+        'yaml': lambda data: yaml.load(data, Loader=yaml.Loader)
     }
     if data_type in PARSING_FUNC:
         return PARSING_FUNC[data_type](data)
